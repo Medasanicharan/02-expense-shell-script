29e2bb9d4f84c3b3e91c5fde8f8e8255da86f932
charan <medasanicharan66@gmail.com>
2025-02-19 19:55:36 +0530

    expense-project

--- backend.sh
+++ backend.sh
@@ -38,10 +38,10 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-id expense
+id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    useradd expense
+    useradd expense &>>$LOGFILE
     VALIDATE $? "Creating expense project"
 else
     echo -e "expense user already created.. $Y SKIPPING $n"
