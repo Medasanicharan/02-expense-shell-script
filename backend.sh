229bfaad879ad54d08fe1bdab4428d967891b99a
charan <medasanicharan66@gmail.com>
2025-02-20 20:12:41 +0530

    expense-project

--- backend.sh
+++ backend.sh
@@ -74,11 +74,11 @@ VALIDATE $? "copied backend service"
 systemctl daemon-reload &>>$LOGFILE
 VALIDATE $? "Demon reload"
 
-systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
-
 systemctl start backend &>>$LOGFILE
 VALIDATE $? "Starting bacend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
